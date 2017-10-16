c2bc7a8ef74f347cb11e1bf6eb00b0f865893d31
empalms <ev@npalmer.io>
2017-10-15 22:15:40 -0500

    implemented addUser post request and forms

--- src/index.js
+++ src/index.js
@@ -2,23 +2,53 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: ''
         }
     }
+
     // prevent component from mounting to the DOM before AJAX request is complete (lifecycle method)
     componentDidMount() {
         this.getUsers()
     }
+
+    // ajax get request for users data
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
         .then((res) => { this.setState({ users: res.data.data.users }); })
         .catch((err) => { console.log(err); })
     }
+
+    // ajax post request to add_user endpoint
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        }
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+        .then((res) => { 
+            this.getUsers();
+            this.setState({ username: '', email: '' }); 
+        })
+        .catch((err) => { console.log(err); })
+    }
+
+    // updates the parent component state with changed form state
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    }
+
+    // render html with state
     render() {
         return (
             <div className="container">
@@ -27,7 +57,13 @@ class App extends Component {
                         <br/>
                         <h1>All Users</h1>
                         <hr/><br/>
-                            <UsersList users={this.state.users}/>
+                        <AddUser 
+                            username={this.state.username}
+                            email={this.state.email}
+                            handleChange={this.handleChange.bind(this)}
+                            addUser={this.addUser.bind(this)}/>
+                        <br/>
+                        <UsersList users={this.state.users}/>
                     </div>
                 </div>
             </div>
